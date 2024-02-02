da3a3242d499b862c8bb94043245e83a00b337aa
jonykarki <jkarki@colostate.edu>
2024-02-01 18:22:07 -0700

    removed R code

--- R/SimpLin.R
+++ R/SimpLin.R
@@ -11,19 +11,19 @@ simpLinR <- function(x, y) {
   print(a)
 }
 
-x <- rnorm(100)
-y <- 1 - 1 * x + rnorm(100)
-simpLinR(x, y)
-
-mod <- lm(y ~ x)
-attributes(lm(y ~ x))
-mod$coefficients
-mod$residuals
-mod$fitted.values
-
-
-X <- cbind(rep(1, 100), x)
-beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
-mse <- (1/98) * sum((y - (X %*% beta_hat))^2)
-
-mse * solve(t(X) %*% X)
+# x <- rnorm(100)
+# y <- 1 - 1 * x + rnorm(100)
+# simpLinR(x, y)
+# 
+# mod <- lm(y ~ x)
+# attributes(lm(y ~ x))
+# mod$coefficients
+# mod$residuals
+# mod$fitted.values
+# 
+# 
+# X <- cbind(rep(1, 100), x)
+# beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
+# mse <- (1/98) * sum((y - (X %*% beta_hat))^2)
+# 
+# mse * solve(t(X) %*% X)
